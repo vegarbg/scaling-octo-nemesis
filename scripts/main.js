6ff4353d8fb425c4488a329c56f0c166cfd7f898
Vegar Guldal <vegar@guldal.no>
2014-05-13 07:26:33 +0200

    Set up RequireJS shims for vendor libraries.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,33 @@
 requirejs.config({
     baseUrl: "scripts",
+    paths: {
+        "signals": "vendor/signals.min",
+        "crossroads": "vendor/crossroads",
+        "hasher": "vendor/hasher",
+    },
+    shim: {
+        "signals": {
+            exports: "signals",
+        },
+        "crossroads": {
+            deps: [ "signals" ],
+            exports: "crossroads",
+        },
+        "hasher": {
+            deps: [ "signals" ],
+            exports: "hasher",
+        },
+    },
 });
 
-(function() {
+requirejs(["crossroads", "hasher"],
+function(crossroads, hasher) {
     var e, n;
     e = document.querySelectorAll("p")[0];
     n = document.createElement("p");
     n.textContent = "Seems to be working.";
     e.parentNode.appendChild(n);
-})();
+
+    console.log(crossroads);
+    console.log(hasher);
+});
