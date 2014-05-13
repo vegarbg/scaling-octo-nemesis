8f35fdcafb7b06ac37a6b39aa71140774492fd24
Vegar Guldal <vegar@guldal.no>
2014-05-13 09:20:10 +0200

    Remove unneeded quotation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 requirejs.config({
     baseUrl: "scripts",
     paths: {
-        "signals": "vendor/signals",
-        "crossroads": "vendor/crossroads",
-        "hasher": "vendor/hasher",
-        "domReady": "vendor/domReady",
+        signals: "vendor/signals",
+        crossroads: "vendor/crossroads",
+        hasher: "vendor/hasher",
+        domReady: "vendor/domReady",
     },
     deps: [ "base" ],
 });
